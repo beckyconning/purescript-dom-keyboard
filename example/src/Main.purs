370081f7b1726563ade4b8c9cba38f0f51d43939
Becky Conning <becky.conning@icloud.com>
2015-12-08 15:35:00 +0000

    Updated example

--- example/src/Main.purs
+++ example/src/Main.purs
@@ -1,39 +1,21 @@
-{-
-Copyright 2015 SlamData, Inc.
-
-Licensed under the Apache License, Version 2.0 (the "License");
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
-
-    http://www.apache.org/licenses/LICENSE-2.0
-
-Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an "AS IS" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
--}
-
 module Main where
 
+import Debug.Trace
 import Prelude
-
 import Control.Monad.Eff (Eff())
-import Control.Monad.Eff.Console (CONSOLE(), log)
-import Control.Monad.Eff.Exception (EXCEPTION())
-import Control.Monad.Eff.Keyboard (onKeyCombination)
 import Control.Monad.Eff.Ref (REF())
-
-import Data.Key (Key(..), NormalKey(), printCombinationApple, normalizeCombination)
-import Data.Set (Set())
-
+import Control.Monad.Eff.Keyboard
+import Control.Monad.Eff.Console (CONSOLE(), log)
+import Data.Key
 import DOM (DOM())
-import DOM.Event.EventTarget (removeEventListener)
+import DOM.HTML.Window
+import DOM.HTML.Types
+import DOM.HTML
+import DOM.Event.EventTarget
 import DOM.Event.EventTypes (keyup, keydown)
-import DOM.HTML (window)
-import DOM.HTML.Types (htmlDocumentToDocument)
-import DOM.HTML.Window (document)
-import DOM.Node.Types (documentToEventTarget)
+import DOM.Node.Types
+import Control.Monad.Eff.Exception (EXCEPTION())
+import Data.Set (Set())
 
 combination1 :: Set NormalKey
 combination1 = normalizeCombination [Key "Alt", Key "Backspace"]
@@ -46,10 +28,14 @@ main = do
   -- Retreive an `EventTarget` using purescript-dom
   doc <- map (htmlDocumentToDocument >>> documentToEventTarget) (window >>= document)
 
+  -- Bind your actions to key combinations, making a reference to the returned
+  -- event listeners if you wish to unbind later.
   onKeyCombination doc (printCombinationApple >>> log) combination1
   listeners <- onKeyCombination doc (\_ -> log "This won't be logged") combination2
 
+  -- Unbind event listeners when you are done with them.
   removeEventListener keydown listeners.keydown false doc
   removeEventListener keyup listeners.keyup false doc
 
   pure unit
+
